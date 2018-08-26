8f5a95edf91f406383ede1b7707a385fafc4c04c
Scott Ferguson <scottferg@outlook.com>
2018-08-25 21:55:28 -0700

    HoldButton subclass to simplify up/dn processing for numeric UI

---
+++ Slider/Slider/CustomControls/HoldButton.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+using Xamarin.Forms;
+
+namespace CamSlider.CustomControls
+{
+    public class HoldButton : Button
+    {
+		Timer timer;
+		bool IsPressed;
+
+		public event EventHandler Held;
+
+		public HoldButton()
+		{
+			timer = new Timer
+			{
+				Enabled = false,
+				Interval = 500
+			};
+			timer.Elapsed += Timer_Elapsed;
+			this.Pressed += HoldButton_Pressed;
+			this.Released += HoldButton_Released;
+		}
+
+		private void HoldButton_Released(object sender, EventArgs e)
+		{
+			IsPressed = false;
+		}
+
+		private void HoldButton_Pressed(object sender, EventArgs e)
+		{
+			Held?.Invoke(this, EventArgs.Empty);
+			IsPressed = true;
+			timer.Enabled = true;
+		}
+
+		private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+		{
+			timer.Interval = 100;
+			if (IsPressed)
+			{
+				Held?.Invoke(this, EventArgs.Empty);
+			}
+			else
+			{
+				timer.Enabled = false;
+				timer.Interval = 500;
+			}
+		}
+	}
+}
--- Slider/Slider/Views/SequencePage.xaml.cs
+++ Slider/Slider/Views/SequencePage.xaml.cs
@@ -15,138 +15,21 @@ namespace CamSlider.Views
 	public partial class SequencePage : ContentPage
 	{
 		SequenceViewModel SeqViewModel;
-		Timer timer;
 
 		public SequencePage ()
 		{
 			InitializeComponent ();
 
 			BindingContext = SeqViewModel = new SequenceViewModel();
-			ButtonMinsUp.Pressed += ButtonMinsUp_Pressed;
-			ButtonMinsUp.Released += Button_Released;
-			ButtonMinsDn.Pressed += ButtonMinsDn_Pressed;
-			ButtonMinsDn.Released += Button_Released;
-			ButtonSecsUp.Pressed += ButtonSecsUp_Pressed;
-			ButtonSecsUp.Released += Button_Released;
-			ButtonSecsDn.Pressed += ButtonSecsDn_Pressed;
-			ButtonSecsDn.Released += Button_Released;
+			ButtonMinsUp.Held += (s, e) => { SeqViewModel.DurationMins++; };
+			ButtonMinsDn.Held += (s, e) => { SeqViewModel.DurationMins--; };
+			ButtonSecsUp.Held += (s, e) => { SeqViewModel.DurationSecs++; };
+			ButtonSecsDn.Held += (s, e) => { SeqViewModel.DurationSecs--; };
 
-			ButtonPMinsUp.Pressed += ButtonPMinsUp_Pressed;
-			ButtonPMinsUp.Released += Button_Released;
-			ButtonPMinsDn.Pressed += ButtonPMinsDn_Pressed;
-			ButtonPMinsDn.Released += Button_Released;
-			ButtonPSecsUp.Pressed += ButtonPSecsUp_Pressed;
-			ButtonPSecsUp.Released += Button_Released;
-			ButtonPSecsDn.Pressed += ButtonPSecsDn_Pressed;
-			ButtonPSecsDn.Released += Button_Released;
-
-			timer = new Timer
-			{
-				Enabled = false,
-				Interval = 500
-			};
-			timer.Elapsed += Timer_Elapsed;
-		}
-
-		enum TimerAction { None, MinsUp, MinsDn, SecsUp, SecsDn, PMinsUp, PMinsDn, PSecsUp, PSecsDn }
-
-		TimerAction Action = TimerAction.None;
-
-		private void Timer_Elapsed(object sender, ElapsedEventArgs e)
-		{
-			timer.Interval = 100;
-			switch (Action)
-			{
-				case TimerAction.None:
-					timer.Enabled = false;
-					timer.Interval = 500;
-					break;
-				case TimerAction.MinsUp:
-					SeqViewModel.DurationMins++;
-					break;
-				case TimerAction.MinsDn:
-					SeqViewModel.DurationMins--;
-					break;
-				case TimerAction.SecsUp:
-					SeqViewModel.DurationSecs++;
-					break;
-				case TimerAction.SecsDn:
-					SeqViewModel.DurationSecs--;
-					break;
-				case TimerAction.PMinsUp:
-					SeqViewModel.PlaybackMins++;
-					break;
-				case TimerAction.PMinsDn:
-					SeqViewModel.PlaybackMins--;
-					break;
-				case TimerAction.PSecsUp:
-					SeqViewModel.PlaybackSecs++;
-					break;
-				case TimerAction.PSecsDn:
-					SeqViewModel.PlaybackSecs--;
-					break;
-			}
-		}
-
-		private void Button_Released(object sender, EventArgs e)
-		{
-			Action = TimerAction.None;
-		}
-
-		private void ButtonMinsUp_Pressed(object sender, EventArgs e)
-		{
-			SeqViewModel.DurationMins++;
-			Action = TimerAction.MinsUp;
-			timer.Enabled = true;
-		}
-
-		private void ButtonMinsDn_Pressed(object sender, EventArgs e)
-		{
-			SeqViewModel.DurationMins--;
-			Action = TimerAction.MinsDn;
-			timer.Enabled = true;
-		}
-
-		private void ButtonSecsUp_Pressed(object sender, EventArgs e)
-		{
-			SeqViewModel.DurationSecs++;
-			Action = TimerAction.SecsUp;
-			timer.Enabled = true;
-		}
-
-		private void ButtonSecsDn_Pressed(object sender, EventArgs e)
-		{
-			SeqViewModel.DurationSecs--;
-			Action = TimerAction.SecsDn;
-			timer.Enabled = true;
-		}
-
-		private void ButtonPMinsUp_Pressed(object sender, EventArgs e)
-		{
-			SeqViewModel.PlaybackMins++;
-			Action = TimerAction.PMinsUp;
-			timer.Enabled = true;
-		}
-
-		private void ButtonPMinsDn_Pressed(object sender, EventArgs e)
-		{
-			SeqViewModel.PlaybackMins--;
-			Action = TimerAction.PMinsDn;
-			timer.Enabled = true;
-		}
-
-		private void ButtonPSecsUp_Pressed(object sender, EventArgs e)
-		{
-			SeqViewModel.PlaybackSecs++;
-			Action = TimerAction.PSecsUp;
-			timer.Enabled = true;
-		}
-
-		private void ButtonPSecsDn_Pressed(object sender, EventArgs e)
-		{
-			SeqViewModel.PlaybackSecs--;
-			Action = TimerAction.PSecsDn;
-			timer.Enabled = true;
+			ButtonPMinsUp.Held += (s, e) => { SeqViewModel.PlaybackMins++; };
+			ButtonPMinsDn.Held += (s, e) => { SeqViewModel.PlaybackMins--; };
+			ButtonPSecsUp.Held += (s, e) => { SeqViewModel.PlaybackSecs++; };
+			ButtonPSecsDn.Held += (s, e) => { SeqViewModel.PlaybackSecs--; };
 		}
 	}
 
